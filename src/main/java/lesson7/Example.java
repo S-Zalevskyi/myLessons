24d166b86c9e2950f1029426633eda35f4ed86cf
S-Zalevskyi <60276234+S-Zalevskyi@users.noreply.github.com>
2020-02-06 01:07:50 +0100

    Learned the basic concepts of access modifiers

---
+++ src/main/java/lesson7/Example.java
@@ -0,0 +1,10 @@
+package lesson7;
+
+public class Example {
+
+    Modifiers modifiers = new Modifiers();
+
+    public Modifiers getModifiers() {
+        return modifiers;
+    }
+}
---
+++ src/main/java/lesson7/Modifiers2.java
@@ -0,0 +1,17 @@
+package lesson7;
+
+public class Modifiers2 {
+
+    public String name;
+    public Modifiers2 () {};
+
+    public class mod2 {
+
+        String happyDay = "Happy Byrthday";
+    }
+
+    public void modification2 () {
+        int day = 14;
+
+    }
+}
